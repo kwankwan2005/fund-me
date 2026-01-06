c0ca035e862d73804a76bb772da960f00c8829a6
Anh-Quan Nguyen <47072821+kwankwan2005@users.noreply.github.com>
2026-01-06 19:35:59 +0700

    dont try to get the API key i've already disabled it

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -5,14 +5,7 @@ import { getDatabase } from "firebase/database";
 
 // modify config here with your Firebase project details
 const firebaseConfig = {
-    apiKey: "AIzaSyBRi-W6o6PGF6VPsRzjBbyAwCFpNFjPQSU",
-    authDomain: "fundme-project.firebaseapp.com",
-    databaseURL: "https://fundme-project-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    projectId: "fundme-project",
-    storageBucket: "fundme-project.firebasestorage.app",
-    messagingSenderId: "798338399983",
-    appId: "1:798338399983:web:4e9675eba2d764cd5ae51a",
-    measurementId: "G-98E2NCWXNY"
+   
 };
 
 const app = initializeApp(firebaseConfig);
